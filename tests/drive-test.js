5c2f9c33450a9cdc147d6fbd7d13f108fdcd7081
danielrearden <danielrearden@gmail.com>
2017-04-16 16:20:03 -0400

    Add test for addFile

--- tests/drive-test.js
+++ tests/drive-test.js
@@ -28,3 +28,22 @@ describe('createFolder', () => {
       .catch(done, done);
   });
 });
+
+describe('addFiles', () => {
+  it('should return an id for the last file copied and moved into new folder', (done) => {
+    app.set('port', '5000');
+    var server = http.createServer(app);
+    server.listen('5000');
+
+    var contractor = new Contractor("Jon", "Snow", true, "danielrearden@google.com");
+    drive.addFiles(contractor, {})
+      .then((result) => {
+        expect(result).to.equal("testid"); // see mock-api.js for value returned by mock API
+      })
+      .then(() => {
+          server.close(done);
+        }
+      )
+      .catch(done, done);
+  });
+});
