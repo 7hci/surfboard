38fb092decee3d8cdcab0aecabfa4786bca6253e
danielrearden <danielrearden@gmail.com>
2017-04-15 22:21:43 -0400

    Add app instance to domain-test

--- tests/domain-test.js
+++ tests/domain-test.js
@@ -1,7 +1,9 @@
 var chai = require('chai');
 var expect = chai.expect;
 var rewire = require('rewire');
+var http = require('http');
 
+var app = require('../app');
 var domain = rewire('../helper/domain');
 var Contractor = require('../model/contractor');
 var mock = require('./mocks');
@@ -10,13 +12,19 @@ domain.__set__('auth', mock.auth);
 
 describe('createContractorEmail', () => {
   it('should return a successful status if a user object is returned', (done) => {
+    app.set('port', '5000');
+    var server = http.createServer(app);
+    server.listen('5000');
+
     var contractor = new Contractor("Jon", "Snow", true, "danielrearden@google.com");
     domain.createContractorEmail(contractor)
       .then((result) => {
         expect(result.status).to.equal("success");
       })
-      .then(done)
+      .then(() => {
+          server.close(done);
+        }
+      )
       .catch(done, done);
   });
-
 });
