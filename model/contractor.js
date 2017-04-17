f649a184bf0a1f56c46434aff0992a9654f74f81
danielrearden <danielrearden@gmail.com>
2017-04-16 22:13:13 -0400

    Updated Contractor class to include override variable

--- model/contractor.js
+++ model/contractor.js
@@ -2,26 +2,32 @@ var crypto = require('crypto');
 
 module.exports =
   class Contractor {
-  constructor(firstName, lastName, isResident, privateEmail) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.isResident = isResident;
-    this.privateEmail = privateEmail;
-  }
+    constructor(firstName, lastName, isResident, privateEmail, override) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.isResident = isResident;
+      this.privateEmail = privateEmail;
+      this.override = override.replace(" ", "");
+    }
 
-  getFullName() {
-    return this.firstName + ' ' + this.lastName;
-  }
+    getFullName() {
+      return this.firstName + ' ' + this.lastName;
+    }
 
-  getEmail() {
-    var pattern = /[^a-zA-Z]/g;
-    var sanitizedFirst = this.firstName.replace(pattern, '').toLowerCase();
-    var sanitizedLast = this.lastName.replace(pattern, '').toLowerCase();
+    getEmail() {
 
-    return sanitizedFirst + '.' + sanitizedLast + '@7hci.com';
-  }
+      if (this.override) {
+        return this.override + '@7hci.com';
+      } else {
+        var pattern = /[^a-zA-Z]/g;
+        var sanitizedFirst = this.firstName.replace(pattern, '').toLowerCase();
+        var sanitizedLast = this.lastName.replace(pattern, '').toLowerCase();
 
-  getPassword() {
-    return crypto.createHash('md5').update(this.getFullName()).digest('hex');
-  }
-};
+        return sanitizedFirst + '.' + sanitizedLast + '@7hci.com';
+      }
+    }
+
+    getPassword() {
+      return crypto.createHash('md5').update(this.getFullName()).digest('hex');
+    }
+  };
